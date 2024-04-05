6aa9b36d074b4ddfbe6e1c650ca6e4c2c8b10eff
Daniel Tejada <dastte93@gmail.com>
2024-04-04 20:00:31 -0500

    correccion de sidebar e imagenes de ordenes

--- src/app/(shop)/checkout/(checkout)/ui/ProductsInCart.tsx
+++ src/app/(shop)/checkout/(checkout)/ui/ProductsInCart.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useCartStore } from "@/store";
 import { currencyFormat } from "@/utils";
+import { ProductImage } from "@/components";
 
 export const ProductsInCart = () => {
 
@@ -32,8 +32,8 @@ export const ProductsInCart = () => {
             {
                 productsInCart.map(product => (
                     <div key={`${product.slug}-${product.size}`} className="flex mb-5">
-                        <Image
-                            src={`/products/${product.image}`}
+                        <ProductImage
+                            src={product.image}
                             width={100}
                             height={100}
                             style={{
--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,7 +1,6 @@
 
 
-import { PaypalButton, Title } from '@/components';
-import Image from 'next/image';
+import { PaypalButton, ProductImage, Title } from '@/components';
 import clsx from 'clsx';
 import { IoCardOutline } from 'react-icons/io5';
 import { getOrderById } from '@/actions';
@@ -65,8 +64,8 @@ export default async function OrderPage({ params }: Props) {
               order!.OrderItem.map(item => (
 
                 <div key={item.product.slug + '-' + item.size} className="flex mb-5">
-                  <Image
-                    src={`/products/${item.product.ProductImage[0].url}`}
+                  <ProductImage
+                    src={item.product.ProductImage[0].url}
                     width={100}
                     height={100}
                     style={{
